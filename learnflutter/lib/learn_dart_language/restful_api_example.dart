c54cd379befe6c98d6f982b3491324a633d51314
HaoNguyen <>
2025-08-24 17:00:43 +0700

    Update RESTful API example

--- learnflutter/lib/learn_dart_language/restful_api_example.dart
+++ learnflutter/lib/learn_dart_language/restful_api_example.dart
@@ -3,63 +3,142 @@ import 'package:flutter/material.dart';
 
 final dio = Dio();
 
-class PostList {
-  List<Post?>? posts;
+class GithubUserList {
+  List<GithubUserInfo?>? users;
 
-  PostList({required this.posts});
+  GithubUserList({required this.users});
 
-  factory PostList.fromJson(List<dynamic> json) {
-    return PostList(posts: json.map((post) => Post.fromJson(post)).toList());
+  factory GithubUserList.fromJson(List<dynamic> json) {
+    return GithubUserList(
+      users: json.map((user) => GithubUserInfo.fromJson(user)).toList(),
+    );
   }
 
   @override
   String toString() {
-    return 'PostList(posts: $posts)';
+    return 'GithubUserList(users: $users)';
   }
 }
 
-class Post {
-  int? userId;
+class GithubUserInfo {
+  // "login": "mojombo",
+  //   "id": 1,
+  //   "node_id": "MDQ6VXNlcjE=",
+  //   "avatar_url": "https://avatars.githubusercontent.com/u/1?v=4",
+  //   "gravatar_id": "",
+  //   "url": "https://api.github.com/users/mojombo",
+  //   "html_url": "https://github.com/mojombo",
+  //   "followers_url": "https://api.github.com/users/mojombo/followers",
+  //   "following_url": "https://api.github.com/users/mojombo/following{/other_user}",
+  //   "gists_url": "https://api.github.com/users/mojombo/gists{/gist_id}",
+  //   "starred_url": "https://api.github.com/users/mojombo/starred{/owner}{/repo}",
+  //   "subscriptions_url": "https://api.github.com/users/mojombo/subscriptions",
+  //   "organizations_url": "https://api.github.com/users/mojombo/orgs",
+  //   "repos_url": "https://api.github.com/users/mojombo/repos",
+  //   "events_url": "https://api.github.com/users/mojombo/events{/privacy}",
+  //   "received_events_url": "https://api.github.com/users/mojombo/received_events",
+  //   "type": "User",
+  //   "user_view_type": "public",
+  //   "site_admin": false
+  String? login;
   int? id;
-  String? title;
-  String? body;
+  String? nodeId;
+  String? avatarUrl;
+  String? gravatarId;
+  String? url;
+  String? htmlUrl;
+  String? followersUrl;
+  String? followingUrl;
+  String? gistsUrl;
+  String? starredUrl;
+  String? subscriptionsUrl;
+  String? organizationsUrl;
+  String? reposUrl;
+  String? eventsUrl;
+  String? receivedEventsUrl;
+  String? type;
+  String? userViewType;
+  bool? siteAdmin;
 
-  Post({this.userId, this.id, this.title, this.body});
+  GithubUserInfo({
+    this.login,
+    this.id,
+    this.nodeId,
+    this.avatarUrl,
+    this.gravatarId,
+    this.url,
+    this.htmlUrl,
+    this.followersUrl,
+    this.followingUrl,
+    this.gistsUrl,
+    this.starredUrl,
+    this.subscriptionsUrl,
+    this.organizationsUrl,
+    this.reposUrl,
+    this.eventsUrl,
+    this.receivedEventsUrl,
+    this.type,
+    this.userViewType,
+    this.siteAdmin,
+  });
 
-  factory Post.fromJson(Map<String, dynamic> json) {
-    return Post(
-      userId: json['userId'] as int?,
+  factory GithubUserInfo.fromJson(Map<String, dynamic> json) {
+    return GithubUserInfo(
+      login: json['login'] as String?,
       id: json['id'] as int?,
-      title: json['title'] as String?,
-      body: json['body'] as String?,
+      nodeId: json['node_id'] as String?,
+      avatarUrl: json['avatar_url'] as String?,
+      gravatarId: json['gravatar_id'] as String?,
+      url: json['url'] as String?,
+      htmlUrl: json['html_url'] as String?,
+      followersUrl: json['followers_url'] as String?,
+      followingUrl: json['following_url'] as String?,
+      gistsUrl: json['gists_url'] as String?,
+      starredUrl: json['starred_url'] as String?,
+      subscriptionsUrl: json['subscriptions_url'] as String?,
+      organizationsUrl: json['organizations_url'] as String?,
+      reposUrl: json['repos_url'] as String?,
+      eventsUrl: json['events_url'] as String?,
+      receivedEventsUrl: json['received_events_url'] as String?,
+      type: json['type'] as String?,
+      userViewType: json['user_view_type'] as String?,
+      siteAdmin: json['site_admin'] as bool?,
     );
   }
 
   @override
   String toString() {
-    return 'Post(userId: $userId, id: $id, title: $title, body: $body)';
+    return '{login: $login, id: $id, nodeId: $nodeId, avatarUrl: $avatarUrl, gravatarId: $gravatarId, url: $url, htmlUrl: $htmlUrl, followersUrl: $followersUrl, followingUrl: $followingUrl, gistsUrl: $gistsUrl, starredUrl: $starredUrl, subscriptionsUrl: $subscriptionsUrl, organizationsUrl: $organizationsUrl, reposUrl: $reposUrl, eventsUrl: $eventsUrl, receivedEventsUrl: $receivedEventsUrl, type: $type, userViewType: $userViewType, siteAdmin: $siteAdmin}';
   }
 }
 
-Future<PostList?> fetchPostsDio() async {
+Future<GithubUserList?> fetchPostsDio() async {
   try {
-    final response = await dio.get(
-      'https://jsonplaceholder.typicode.com/posts',
-    );
+    final response = await dio.get('https://api.github.com/users');
     if (response.statusCode == 200) {
-      return PostList.fromJson(response.data);
+      return GithubUserList.fromJson(response.data);
     } else {
       throw Exception('Server error: ${response.statusCode}');
     }
   } on DioException catch (e) {
     throw Exception('Connect error: ${e.message}');
   } catch (e) {
-    debugPrint('Error: $e');
     throw Exception('$e');
   }
 }
 /*
 void main() {
-  fetchPostsDio();
+    try {
+    var postList = await fetchPostsDio();
+    if (postList?.users != null) {
+      var posts = postList?.users;
+      // debugPrint('Posts: ${posts![0]}');
+      debugPrint('Posts: ${posts!}');
+    } else {
+      debugPrint('Posts is null');
+    }
+  } catch (error) {
+    debugPrint('Error: ${error.toString()}');
+  }
 }
 */
